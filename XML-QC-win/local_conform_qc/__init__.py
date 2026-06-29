bd3fa78e8775f0725307674300ce03940d93b132
JIU Sun <1653036702@qq.com>
2026-06-29 17:28:36 +0800

    Add Windows and macOS XML-QC variants

---
+++ XML-QC-win/local_conform_qc/__init__.py
@@ -0,0 +1,12 @@
+"""Modular local conform QC package."""
+
+__all__ = [
+    "gui_picker",
+    "logging_utils",
+    "media_scanner",
+    "models",
+    "qc_engine",
+    "report_writer",
+    "resolve_api",
+    "xml_parser",
+]
